62676b6140af34842a47965beb8aa5ee97b05fb6
zaimo3869 <zaimo3869@gmail.com>
2022-03-13 19:21:45 +0100

    Update authentification.js

--- middleware/authentification.js
+++ middleware/authentification.js
@@ -5,15 +5,15 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, `${process.env.TOKEN_KEY}`);
-    const userId = decodedToken.userId;
-    console.log(req.body);
+    const userId = decodedToken.userId; 
+    res.userId = userId;
     if (req.body.userId && req.body.userId !== userId) {
-      throw "User ID non valable !";
+      res.status(401).json("Utilisateur non autorisé à supprimer / modifier");
     } else {
-        res.userId=userId;
       next();
     }
   } catch (error) {
+    console.log(error);
     res.status(401).json({ error: error | "Requête non-authentifiée !" });
   }
 };
